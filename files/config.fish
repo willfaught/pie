3c5da12807db155a0251ba4f8d6ff200151795c5
Will Faught <willfaught@users.noreply.github.com>
2020-03-10 15:03:42 -0700

    Remove ruby paths from PATH

--- files/config.fish
+++ files/config.fish
@@ -12,7 +12,7 @@ set -gx HISTSIZE '32768'
 set -gx LANG 'en_US.UTF-8'
 set -gx LC_ALL 'en_US.UTF-8'
 set -gx NODE_REPL_HISTORY_SIZE '32768'
-set -gx PATH /usr/local/opt/ruby/bin $PATH $GOPATH/bin /usr/local/lib/ruby/gems/2.6.1/bin
+set -gx PATH $PATH $GOPATH/bin
 set -gx PYTHONIOENCODING 'UTF-8'
 set -gx fish_greeting
 
