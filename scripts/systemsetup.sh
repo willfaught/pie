57fddf62f7baebced29e410c89d42a0c93e07201
Will Faught <willfaught@users.noreply.github.com>
2020-08-02 02:12:59 -0700

    Remove computer name config

--- scripts/systemsetup.sh
+++ scripts/systemsetup.sh
@@ -3,13 +3,11 @@
 set -x
 
 sudo systemsetup -setallowpowerbuttontosleepcomputer on
-sudo systemsetup -setcomputername "Will's Mac"
 sudo systemsetup -setcomputersleep Never
 sudo systemsetup -setdisablekeyboardwhenenclosurelockisengaged no
 sudo systemsetup -setdisplaysleep 60
 sudo systemsetup -setharddisksleep 10
 sudo systemsetup -setkernelbootarchitecture default
-sudo systemsetup -setlocalsubnetname 'Wills-Mac'
 sudo systemsetup -setnetworktimeserver time.apple.com
 sudo systemsetup -setrestartfreeze on
 sudo systemsetup -setrestartpowerfailure on
