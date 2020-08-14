a84b9e9432b580c3056935e54b3881f9ebf9b7ce
Will Faught <willfaught@users.noreply.github.com>
2020-08-13 18:39:19 -0700

    Move comment to end of line

--- scripts/lsregister.sh
+++ scripts/lsregister.sh
@@ -2,5 +2,4 @@
 
 set -x
 
-# Remove duplicates in the "Open With" menu
-/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user # Remove duplicates in the "Open With" menu
