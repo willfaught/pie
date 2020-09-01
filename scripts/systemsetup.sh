f840242363137dc07f57a422620fce6828452420
Will Faught <willfaught@users.noreply.github.com>
2020-08-31 18:07:44 -0700

    Remove restartpowerfailure config

--- scripts/systemsetup.sh
+++ scripts/systemsetup.sh
@@ -9,7 +9,6 @@ sudo systemsetup -setharddisksleep 10
 sudo systemsetup -setkernelbootarchitecture default
 sudo systemsetup -setnetworktimeserver time.apple.com
 sudo systemsetup -setrestartfreeze on
-sudo systemsetup -setrestartpowerfailure on
 sudo systemsetup -settimezone 'America/Los_Angeles'
 sudo systemsetup -setusingnetworktime on
 sudo systemsetup -setwaitforstartupafterpowerfailure 0
