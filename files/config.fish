3bddd2c6f980dd2f24738879c4924ddbd715757a
Will Faught <willfaught@users.noreply.github.com>
2021-09-02 18:36:31 -0700

    Disable *env scripts

--- files/config.fish
+++ files/config.fish
@@ -274,9 +274,9 @@ end
 
 # Setup
 
-status --is-interactive; and source (nodenv init - | psub)
-status --is-interactive; and source (pyenv init - | psub)
-status --is-interactive; and source (rbenv init - | psub)
+# status --is-interactive; and source (nodenv init - | psub)
+# status --is-interactive; and source (pyenv init - | psub)
+# status --is-interactive; and source (rbenv init - | psub)
 
 # Local
 
