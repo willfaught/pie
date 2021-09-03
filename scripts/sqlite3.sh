3edc8f926feaae409c1b498b2c90112930ea2e45
Will Faught <willfaught@users.noreply.github.com>
2021-09-03 16:50:28 -0700

    Remove scripts/sqlite3.sh

--- scripts/sqlite3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -x
-
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '/Library/Desktop Pictures/Milky Way.jpg'" # Set the wallpaper
-
-killall Dock
