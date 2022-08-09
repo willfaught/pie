4c7469156159cc8f7d05535d6fcfb9f420b188d4
Will Faught <willfaught@users.noreply.github.com>
2022-08-09 10:46:00 -0700

    Remove networktimeserver setting

--- scripts/systemsetup.sh
+++ scripts/systemsetup.sh
@@ -7,7 +7,6 @@ sudo systemsetup -setdisablekeyboardwhenenclosurelockisengaged no
 sudo systemsetup -setdisplaysleep 60
 sudo systemsetup -setharddisksleep 10
 sudo systemsetup -setkernelbootarchitecture default
-sudo systemsetup -setnetworktimeserver time.apple.com
 sudo systemsetup -setrestartfreeze on
 sudo systemsetup -settimezone 'America/Los_Angeles'
 sudo systemsetup -setusingnetworktime on
