ab34a86d7a64afd61b3e6b524f21a3768553826c
Will Faught <willfaught@users.noreply.github.com>
2022-08-09 11:24:33 -0700

    Remove lsregister.sh

--- scripts/lsregister.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -x
-
-/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user # Remove duplicates in the "Open With" menu
