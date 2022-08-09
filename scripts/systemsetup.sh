5aa677c27bc606e852e79aff1a9f79b32b34f961
Will Faught <willfaught@users.noreply.github.com>
2022-08-09 10:47:11 -0700

    Remove kernelbootarchitecture setting

--- scripts/systemsetup.sh
+++ scripts/systemsetup.sh
@@ -6,7 +6,6 @@ sudo systemsetup -setcomputersleep Never
 sudo systemsetup -setdisablekeyboardwhenenclosurelockisengaged no
 sudo systemsetup -setdisplaysleep 60
 sudo systemsetup -setharddisksleep 10
-sudo systemsetup -setkernelbootarchitecture default
 sudo systemsetup -setrestartfreeze on
 sudo systemsetup -settimezone 'America/Los_Angeles'
 sudo systemsetup -setusingnetworktime on
