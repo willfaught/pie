23b747a441c725b14f7b77bc3ca522bc27b1fcb0
Will Faught <willfaught@users.noreply.github.com>
2023-01-21 17:53:28 -0800

    Add lines-drop-while func

--- files/config.fish
+++ files/config.fish
@@ -188,6 +188,10 @@ function lines-drop
   awk "{l[NR] = \$0} END {for (i=1+$argv[1]; i<=NR; i++) print l[i]}"
 end
 
+function lines-drop-while
+  awk "BEGIN { start = 0; } { if (\$0 !~ /$argv[1]/) { start = 1; } } { if (start) print \$0; }"
+end
+
 function lines-drop-last
   awk "{l[NR] = \$0} END {for (i=1; i<=NR-$argv[1]; i++) print l[i]}"
 end
