ebc30dccebc6c25c3fca43d54ddd74fab130acd0
Will Faught <willfaught@users.noreply.github.com>
2025-10-15 23:05:19 -0700

    Change domains to lowercase

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -5,24 +5,24 @@ set -x
 defaults write -g AppleKeyboardUIMode -int 2 # Enable full keyboard access for all controls
 defaults write -g AppleShowAllExtensions -bool true # Show all filename extensions
 
-defaults -currentHost write com.apple.Safari AutoFillCreditCardData -bool false # Disable AutoFill
-defaults -currentHost write com.apple.Safari AutoFillFromAddressBook -bool false # Disable AutoFill
-defaults -currentHost write com.apple.Safari AutoFillMiscellaneousForms -bool false # Disable AutoFill
-defaults -currentHost write com.apple.Safari AutoFillPasswords -bool false # Disable AutoFill
-defaults -currentHost write com.apple.Safari AutoOpenSafeDownloads -bool false # Don't open safe downloaded files automatically
-defaults -currentHost write com.apple.Safari IncludeDevelopMenu -bool true # Show the Develop menu
-defaults -currentHost write com.apple.Safari SendDoNotTrackHTTPHeader -bool true # Enable Do Not Track
-defaults -currentHost write com.apple.Safari ShowFullURLInSmartSearchField -bool true # Show the full URL in the address bar
-defaults -currentHost write com.apple.Safari ShowIconsInTabs -bool true # Show favicons in tabs
-defaults -currentHost write com.apple.Safari WebKitTabToLinksPreferenceKey -bool true # Press tab to highlight each item on a web page
+defaults -currentHost write com.apple.safari AutoFillCreditCardData -bool false # Disable AutoFill
+defaults -currentHost write com.apple.safari AutoFillFromAddressBook -bool false # Disable AutoFill
+defaults -currentHost write com.apple.safari AutoFillMiscellaneousForms -bool false # Disable AutoFill
+defaults -currentHost write com.apple.safari AutoFillPasswords -bool false # Disable AutoFill
+defaults -currentHost write com.apple.safari AutoOpenSafeDownloads -bool false # Don't open safe downloaded files automatically
+defaults -currentHost write com.apple.safari IncludeDevelopMenu -bool true # Show the Develop menu
+defaults -currentHost write com.apple.safari SendDoNotTrackHTTPHeader -bool true # Enable Do Not Track
+defaults -currentHost write com.apple.safari ShowFullURLInSmartSearchField -bool true # Show the full URL in the address bar
+defaults -currentHost write com.apple.safari ShowIconsInTabs -bool true # Show favicons in tabs
+defaults -currentHost write com.apple.safari WebKitTabToLinksPreferenceKey -bool true # Press tab to highlight each item on a web page
 
-defaults -currentHost write com.apple.AppleMultitouchTrackpad Clicking -bool true # System Preferences -> Trackpad -> Point & Click -> Tap to click
+defaults -currentHost write com.apple.applemultitouchtrackpad Clicking -bool true # System Preferences -> Trackpad -> Point & Click -> Tap to click
 
-defaults -currentHost write com.apple.Terminal ShowLineMarks -bool false # Hide line marks
-defaults write com.apple.Terminal 'Default Window Settings' -string 'VS Code Dark Plus' # Default profile
-defaults write com.apple.Terminal 'Startup Window Settings' -string 'VS Code Dark Plus' # Default profile
+defaults -currentHost write com.apple.terminal ShowLineMarks -bool false # Hide line marks
+defaults write com.apple.terminal 'Default Window Settings' -string 'VS Code Dark Plus' # Default profile
+defaults write com.apple.terminal 'Startup Window Settings' -string 'VS Code Dark Plus' # Default profile
 
-defaults -currentHost write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true # System Preferences -> Trackpad -> Point & Click -> Tap to click
+defaults -currentHost write com.apple.driver.applebluetoothmultitouch.trackpad Clicking -bool true # System Preferences -> Trackpad -> Point & Click -> Tap to click
 
 defaults -currentHost write com.apple.dock autohide -bool true # Automatically hide and show the dock
 defaults -currentHost write com.apple.dock expose-group-apps -bool false # Don't group windows by app in Expose
